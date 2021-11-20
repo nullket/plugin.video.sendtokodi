ae7736d3f4364e1df09e1abfaafeab37039d95df
nullket <25816243+nullket@users.noreply.github.com>
2021-11-20 14:12:16 +0100

    Init new build with local xml file

--- .github/scripts/build-addon.sh
+++ .github/scripts/build-addon.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 set -e
 
-REPO_NAME=$1
-REPO_URL=$2
+KODI_TARGET=$1
 
 # check if version bump failed (will be "undefined" according to mathieudutour/github-tag-action@v5.6)
-if [[ "$VERSION" == "undefined" ]]; then
-    echo "Failed to bump version. Abborting build."
+if [[ "$PLUGIN_VERSION" == "undefined" || -z "$PLUGIN_VERSION" ]]; then
+    echo "Failed to bump or get version. Abborting build."
     exit 190
 fi
 
-# clone release repo (just needed for the correct addon.xml, TODO decide if addon.xml should be in the main development repo and not the release repo)
-cd ..
-git clone https://$REPO_URL
+# based on the targeting kodi version different adjustments must be made. Further information here: https://kodi.wiki/view/Addon.xml
+# xmlstarlet or a python xml wrapper would be more robust than sed
+case $KODI_TARGET in
+  "Leia")
+    echo -n "TODO"
+    addon_xml_adjuster.py --plugin-version $PLUGIN_VERSION --xbmc-python "2.26.0"
+    build_folder=Leia
+    ;;
 
-# package addon
-envsubst < "$REPO_NAME/$ADDON_NAME/addon.template.xml" > "$REPO_NAME/$ADDON_NAME/addon.xml" # addon.xml
-cp $REPO_NAME/$ADDON_NAME/addon.xml $ADDON_NAME # copy addon.xml to addon folder
-zip -r $REPO_NAME/$ADDON_NAME/$ADDON_NAME-$VERSION.zip $ADDON_NAME -x "*.git*" # create zip
+  "Matrix" | "Nexus")
+    addon_xml_adjuster.py --plugin-version $PLUGIN_VERSION --xbmc-python "3.0.0"
+    build_folder=Matrix
+    ;;
+  *)
+    echo -n "Unknown kodi version as target. Aborting build."
+    exit 191
+    ;;
+esac
+
+mkdir ../${build_folder}
+zip -r ../${build_folder}/plugin.video.sendtokodi-$PLUGIN_VERSION.zip . -x "*.git*" # create zip
