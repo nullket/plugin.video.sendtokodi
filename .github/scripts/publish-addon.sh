1e1c908abe74a822b44cc35b6ec9688c037b7626
nullket <25816243+nullket@users.noreply.github.com>
2021-11-20 16:08:26 +0100

    splitted building and publishing even more - fix 15

--- .github/scripts/publish-addon.sh
+++ .github/scripts/publish-addon.sh
@@ -8,26 +8,23 @@ REPO_URL=$2
 echo "DEBUG $1 DEBUG $2"
 REPO_FOLDER=release_repo
 # check if action was triggered in a fork and avoid trying to push. FOR TESTING DISABLED
-if [[ $GITHUB_REPOSITORY == "firsttris/plugin.video.sendtokodi" || true ]]; then
+if [[ $GITHUB_REPOSITORY == "firsttris/plugin.video.sendtokodi" ]]; then
     # clone repo
     git clone https://$REPO_URL $REPO_FOLDER
 
-    # add the created zip file
-    ls -lah
+    # add the plugin zip file
     mv plugin.video.sendtokodi-$VERSION.zip $REPO_FOLDER/plugin.video.sendtokodi/
 
     # Update repository addon xml (not the plugin addon.xml) to include the latest version of sendtokodi
     cd $REPO_FOLDER
     envsubst < "addon.template.xml" > "addon.xml"  
     md5sum addon.xml > addon.xml.md5
-    # Add new addon zip file and repo addon.xml, its md5 hash file then commit and push
+    # Add new addon zip file, the repo addon.xml, its md5 hash file and then commit and push
     git config --global user.name "github-actions[bot]"
     git config --global user.email "github-actions[bot]@users.noreply.github.com"
     git add .
     git commit -m "CI Update for $VERSION"
-    #git push --force --quiet "https://firsttris:$TOKEN@$REPO_URL" master
-    echo "TESTING: WOULD PUSH HERE ABBORTING FOR TEST"
-    git status
+    git push --force --quiet "https://firsttris:$TOKEN@$REPO_URL" master
 else 
     echo "Not in the main repo, build will not be published."
 fi 
